ebabcd9a32f17f80a13c1f9e71d382d81b094cf6
simpsonc6@winthropalumni.com <simpsonc6@winthropalumni.com>
2022-11-01 10:42:10 -0400

    Created Dashboard, Login, Register features in angular

---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+  home:any = FormGroup;
+  constructor(private router:Router) { }
+  
+  ngOnInit(): void {
+  }
+  toRegister(){
+    this.router.navigate(['register'])
+  }
+
+  toLogin(){
+    this.router.navigate(['login']);
+  }
+}
---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  register:any = FormGroup;
+
+  constructor(private fb:FormBuilder, private router:Router) { }
+
+  ngOnInit(): void {
+    this.register = this.fb.group({
+      firstname:['',Validators.required],
+      lastname:['',Validators.required],
+      username:['',Validators.required],
+      email:['',Validators.compose([Validators.required,Validators.email])],
+      password:['',Validators.required]
+      
+    });
+  }
+  regSubmit(data:any){
+
+    console.log(data);
+
+
+  }
+  toLogin(){
+    this.router.navigate(['login']);
+  }
+
+  toDash(){
+    this.router.navigate(['home']);
+  }
+
+}
