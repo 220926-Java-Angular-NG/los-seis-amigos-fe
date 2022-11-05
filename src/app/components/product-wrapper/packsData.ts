d0def6fab490fad1acaddaa33f162c0c5ea5d496
Jonathan Bedoy <jonathanbedoy1@gmail.com>
2022-11-05 09:43:03 -0700

    styling adjustments

--- src/app/components/product-wrapper/packsData.ts
+++ src/app/components/product-wrapper/packsData.ts
@@ -6,63 +6,72 @@ export const SAMPLE_PACKS: Pack[] = [
     price:5.99,
     setName:'Rivals of Ixalan',
     imgSrc: 'https://m.media-amazon.com/images/I/51QPXUGAnqL._AC_SY580_.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 14
   },
   {
     id: 2,
     price:5.99,
     setName:'Ixalan',
     imgSrc: 'https://m.media-amazon.com/images/I/41dvX9nI+eS._AC_SY580_.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 15
   },
   {
     id: 3,
     price:5.99,
     setName:'Ice Age',
     imgSrc: 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/1110897.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 13
   },
   {
     id: 4,
     price:5.99,
     setName:'Alliances',
     imgSrc: 'https://i.ebayimg.com/images/g/Ur8AAOSw85Rivgj-/s-l500.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 14
   },
   {
     id: 5,
     price:5.99,
     setName:'Coldsnap',
     imgSrc: 'https://52f4e29a8321344e30ae-0f55c9129972ac85d6b1f4e703468e6b.ssl.cf2.rackcdn.com/products/pictures/141888.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 11
   },
   {
     id: 6,
     price:5.99,
     setName:'Mirage',
     imgSrc: 'https://i.ebayimg.com/images/g/FQUAAOSw1a1g1gDa/s-l640.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 12
   },
   {
     id: 7,
     price:5.99,
     setName:'Visions',
     imgSrc: 'https://crystal-cdn4.crystalcommerce.com/photos/30678/CnrmyF2kmtqMCyvn.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 13
   },
   {
     id: 8,
     price:5.99,
     setName:'Weatherlight',
     imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCV6wU_dn8RH8TJc66n78lTHposKWkbuOXCQ&usqp=CAU',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 14
   },
   {
     id: 9,
     price:5.99,
     setName:'Tempest',
     imgSrc: 'https://m.media-amazon.com/images/I/51iJIWVA9WL._AC_SY580_.jpg',
-    description: `Includes 10 random cards from the `
+    description: `Includes 10 random cards from the `,
+    cardCount: 12
   },
 
 ]
