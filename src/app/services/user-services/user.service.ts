988264c8d7e00374a331eb40bd3e89361a79f22b
Chase Lopez <chase.opez16@gmail.com>
2022-11-09 21:38:31 -0600

    refactored login to give user a message for wrong username-password

--- src/app/services/user-services/user.service.ts
+++ src/app/services/user-services/user.service.ts
@@ -98,6 +98,7 @@ export class UserService {
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send the error to remote logging infrastructure
+      alert("User name does not exist!")
       console.error(error); // log to console instead
       // Let the app keep running by returning an empty result.
       return of(result as T);
